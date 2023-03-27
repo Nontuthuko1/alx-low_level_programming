b268a5fd840dd61a17f9ca2da9778ced6ec14098
myUsername <myemail@gmail.com>
2023-03-27 04:46:53 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
-  * resert_to_98 - Takes a pointer to an int and updates
-  * the value it points to 98
+  * reset_to_98 - Takes a pointer to an int and updates
+  * the value it points to to 98
   *
   * @n: An interger iput pointer
   *
   * Return : nothing
   */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
