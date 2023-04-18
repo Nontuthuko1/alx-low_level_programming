456919abd6e59a6f81babd4e48d7fe33052e804d
myUsername <myemail@gmail.com>
2023-04-17 19:13:37 -0700

    tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,12 +3,14 @@
 
  /**
   * struct dog - a dog's basic info
+  *
   * @name: First member
   * @age: Second member
   * @owner: Third member
   *
   * Description: Longer description
   */
+
 struct dog
 {
 	char *name;
