b513b5192b1469b34dd329901c1ef0cd4bdd16fa
myUsername <myemail@gmail.com>
2023-04-18 03:40:03 -0700

    task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
- /**
-  * struct dog - a dog's basic info
-  *
-  * @name: First member
-  * @age: Second member
-  * @owner: Third member
-  *
-  * Description: Longer description
-  */
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
@@ -18,8 +17,8 @@ struct dog
 };
 
 /**
-  * dog_t - typedef for struct dog
-  */
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -28,4 +27,5 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 #endif
